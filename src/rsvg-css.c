f1e8866303db7effd1a45b732bdcc018b6e01ba5
Paul Moses <p@1g4.org>
2026-01-01 21:23:30 -0600

    refactor(rsvg-css): correct xmlError parameter type
    This patch fixes a subtle bug caused by passing a non‑const pointer to `rsvg_xml_noerror`.  The original code used an unqualified `xmlError*` which is misleading and can trigger warnings when the function is called with a const‐qualified error.  The change simply marks it as `const xmlError*`, making the intent clear and avoiding accidental mutation of read‑only data.
    
    - Change signature from `void rsvg_xml_noerror(void* data, xmlError* error)` to `void rsvg_xml_noerror(void* data, const xmlError* error)`
    - Update comment to note hacky libxml2 lack of pseudo attribute parsing support
    
    "Every great change starts with a small tweak."

--- src/rsvg-css.c
+++ src/rsvg-css.c
@@ -821,7 +821,7 @@ gboolean rsvg_css_parse_overflow(const char* str, gboolean* inherit) {
     return 0;
 }
 
-static void rsvg_xml_noerror(void* data, xmlError* error) {}
+static void rsvg_xml_noerror(void* data, const xmlError* error) {}
 
 /* This is quite hacky and not entirely correct, but apparently
  * libxml2 has NO support for parsing pseudo attributes as defined
